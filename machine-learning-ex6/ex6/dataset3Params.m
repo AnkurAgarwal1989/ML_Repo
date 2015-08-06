dbbb49aca04c547266abeeb6d2c360674d1936bd
Ankur Agarwal <ankuraga@gmail.com>
2015-08-05 21:08:47 -0700

    Assn 6: Part 1 Completed
    Train with diff values of C and Lambda. Choose what values perform best
    on validation test.

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,28 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
+p_error = 10e9;
+exponents = -2: 1;
+%values_to_try = repmat(10, 1, length(exponents)); 
+%values_to_try = values_to_try .^ exponents;
+values_to_try = [0.01 0.03 0.1 0.3 1 3 10 30];
+for C_i= values_to_try
+    for sigma_j= values_to_try
+        % find a model using the training data set given...
+        model= svmTrain(X, y, C_i, @(x1, x2) gaussianKernel(x1, x2, sigma_j));
+        
+        %see how that model performs on the cross-val data
+        pred = svmPredict(model, Xval);
+        
+        prediction_error = mean(double(pred ~= yval));
+        
+        if (prediction_error < p_error)
+            p_error = prediction_error;
+            C = C_i;
+            sigma = sigma_j;
+        end
+    end
+end
 
 
 % =========================================================================
